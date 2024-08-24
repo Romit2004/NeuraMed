f3eb757cf2a194b29451c0f4be4dfc3049636c69
Romit2004 <romitguha7@gmail.com>
2024-08-25 01:03:41 +0530

    modified:   Frontend/src/Components/Forms/Pneumonia/Pneumonia.jsx

--- Frontend/src/Components/Forms/Pneumonia/Pneumonia.jsx
+++ Frontend/src/Components/Forms/Pneumonia/Pneumonia.jsx
@@ -68,20 +68,6 @@ const Pneumonia = () => {
                     Disease Predictor
                 </h1>
 
-                {/* <input
-                        className="flex h-10 w-full md:w-[50vw] rounded-md border border-white bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 mb-2"
-                        type="file"
-                    
-                        multiple
-                        onChange={onFileChange} 
-                        accept="image/*"
-                        ref={inputRef}
-                    /> */}
-                <div className="flex items-center justify-center w-full bg-white bg-opacity-10 mb-6">
-                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white bg-opacity-10  dark:border-white dark:hover:border-gray-500 hover:bg-white hover:bg-opacity-5">
-                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                            <svg className="w-8 h-8 mb-4 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                 
                 <div class="flex items-center justify-center w-full bg-white bg-opacity-10 mb-6">
                     <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white bg-opacity-10  dark:border-white dark:hover:border-gray-500 hover:bg-white hover:bg-opacity-5">
@@ -89,11 +75,11 @@ const Pneumonia = () => {
                             <svg class="w-8 h-8 mb-4 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                             </svg>
-                            <p className="mb-2 text-sm text-white dark:text-white"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                            <p className="text-xs text-white dark:text-white">SVG, PNG or JPG </p>
+                            <p class="mb-2 text-sm text-white dark:text-white"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p class="text-xs text-white dark:text-white">SVG, PNG or JPG </p>
                         </div>
                         
-                        <input id="dropzone-file" type="file" className="hidden" onChange={onFileChange}
+                        <input id="dropzone-file" type="file" class="hidden" onChange={onFileChange}
                             accept="image/*"
                             ref={inputRef} />
                     </label>
@@ -134,7 +120,7 @@ const Pneumonia = () => {
                 {prediction && (
                     <div>
                         <h1>Prediction: {prediction.prediction}</h1>
-                    
+                        <p>Confidence: {prediction.confidence.toFixed(2)}</p>
                     </div>
                 )}
             </div>
