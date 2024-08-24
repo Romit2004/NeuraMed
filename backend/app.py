8ce631b19984b39be57980cf7f1594bf7ca8c307
Snaju003 <117941685+Snaju003@users.noreply.github.com>
2024-08-24 15:39:14 +0530

    modified:   backend/app.py 	new file:   backend/uploads/IM-0007-0001.jpeg 	new file:   backend/uploads/WhatsApp_Image_2024-08-24_at_3.37.49_PM.jpeg 	new file:   backend/uploads/WhatsApp_Image_2024-08-24_at_3.37.50_PM_1.jpeg 	new file:   backend/uploads/WhatsApp_Image_2024-08-24_at_3.37.50_PM_2.jpeg 	renamed:    backend/models/parkinsons.csv -> models/parkinsons.csv 	renamed:    backend/models/parkinsons_model.pkl -> models/parkinsons_model.pkl 	renamed:    backend/models/pneumonia_detection_model.h5 -> models/pneumonia_detection_model.h5 	renamed:    backend/models/scaler.pkl -> models/scaler.pkl 	renamed:    backend/models/train_model.py -> models/train_model.py

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,33 @@
-from flask import Flask, request, jsonify # type: ignore
-from flask_cors import CORS # type: ignore
+from flask import Flask, request, jsonify 
+from flask_cors import CORS
 import os
-from werkzeug.utils import secure_filename # type: ignore
-import numpy as np # type: ignore
-import tensorflow as tf # type: ignore
-from PIL import Image # type: ignore
-from tensorflow.keras.applications.efficientnet import preprocess_input # type: ignore
-import cv2 # type: ignore
-from tensorflow.keras.models import load_model # type: ignore
+from werkzeug.utils import secure_filename 
+import numpy as np 
+import tensorflow as tf 
+from PIL import Image 
+from tensorflow.keras.applications.efficientnet import preprocess_input 
+import cv2 
 
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
-model_pneumonia=tf.load_model('')
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+model_pneumonia = tf.keras.models.load_model('../models/pneumonia_detection_model.h5')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def prepare_image(filepath):
+    img_size = 200
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    resized_img = cv2.resize(img_array, (img_size, img_size))
+    resized_img = resized_img.reshape(-1, img_size, img_size, 1) / 255.0
+    return resized_img
 
 @app.route('/pneumonia', methods=['POST'])
 def upload_file():
