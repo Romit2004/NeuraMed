2dad7ec66773ed76d65e8e10a88bccf56fa25a89
Swastik <120981863+swvstik@users.noreply.github.com>
2024-08-25 13:12:38 +0530

    modified:   Frontend/src/Components/Header/Header.jsx

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -20,27 +20,28 @@ function Header() {
   };
 
   return (
-    <div className='relative w-full bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg z-20 shadow-3xl'>
-      <div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8'>
-        <div className='inline-flex items-center space-x-2'>
+    <div className="relative w-full bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg z-20 shadow-3xl">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+        <div className="inline-flex items-center space-x-2">
           <span>
             <img
-              width='30'
-              height='30'
-              viewBox='0 0 50 56'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-              src='public\logo.jpeg'
+              width="30"
+              height="30"
+              viewBox="0 0 50 56"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              src="public\logo.jpeg"
             />
           </span>
+          <label className="font-bold text-white">NeuraMed</label>
         </div>
-        <div className='hidden grow items-start lg:flex'>
-          <ul className='ml-12 inline-flex space-x-8'>
+        <div className="hidden grow items-start lg:flex">
+          <ul className="ml-12 inline-flex space-x-8">
             {menuItems.map((item) => (
               <li key={item.name}>
                 <a
                   href={item.href}
-                  className='inline-flex items-center text-lg font-bold text-white hover:text-gray-900 hover:underline'
+                  className="inline-flex items-center text-lg font-bold text-white hover:text-gray-900 hover:underline"
                 >
                   {item.name}
                   <span></span>
@@ -50,46 +51,47 @@ function Header() {
           </ul>
         </div>
 
-        <div className='lg:hidden'>
-          <Menu onClick={toggleMenu} className='h-6 w-6 cursor-pointer' />
+        <div className="lg:hidden">
+          <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
         </div>
         {isMenuOpen && (
-          <div className='absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden'>
-            <div className='divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5'>
-              <div className='px-5 pb-6 pt-5'>
-                <div className='flex items-center justify-between'>
-                  <div className='inline-flex items-center space-x-2'>
+          <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
+            <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="px-5 pb-6 pt-5">
+                <div className="flex items-center justify-between">
+                  <div className="inline-flex items-center space-x-2">
                     <span>
                       <img
-                        width='30'
-                        height='30'
-                        viewBox='0 0 50 56'
-                        fill='none'
-                        xmlns='http://www.w3.org/2000/svg'
-                        src='public\logo.jpeg'
+                        width="30"
+                        height="30"
+                        viewBox="0 0 50 56"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        src="public\logo.jpeg"
                       />
                     </span>
+                    <label className="font-bold text-black">NeuraMed</label>
                   </div>
-                  <div className='-mr-2'>
+                  <div className="-mr-2">
                     <button
-                      type='button'
+                      type="button"
                       onClick={toggleMenu}
-                      className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
+                      className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                     >
-                      <span className='sr-only'>Close menu</span>
-                      <X className='h-6 w-6' aria-hidden='true' />
+                      <span className="sr-only">Close menu</span>
+                      <X className="h-6 w-6" aria-hidden="true" />
                     </button>
                   </div>
                 </div>
-                <div className='mt-6'>
-                  <nav className='grid gap-y-4'>
+                <div className="mt-6">
+                  <nav className="grid gap-y-4">
                     {menuItems.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className='-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50'
+                        className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50"
                       >
-                        <span className='ml-3 text-base font-medium text-gray-900'>
+                        <span className="ml-3 text-base font-medium text-gray-900">
                           {item.name}
                         </span>
                         <span></span>
